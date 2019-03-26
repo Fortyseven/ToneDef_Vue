b429e9e991ba071cf73a672b86cc46a6af3d355a
Toby Deshane <toby.deshane@iinteractive.com>
2019-03-26 12:49:01 -0400

    Revised extras layout; merged in tones from side project

--- src/components/extras/ExtrasUSPanel.vue
+++ src/components/extras/ExtrasUSPanel.vue
@@ -2,24 +2,48 @@
 
 <template>
     <div id="ExtrasUSPanel">
-        <div class="pad-row">
-            <ToneSequenceButton name="Busy" :context="audio_context">
-                <SequenceNode freqs="480,620" duration="500"></SequenceNode>
-                <SequenceNode freqs="0" duration="500"></SequenceNode>
-            </ToneSequenceButton>
-            <ToneSequenceButton name="Off Hook" :context="audio_context">
-                <SequenceNode freqs="1440,2060,2450,2600" duration="100"></SequenceNode>
-                <SequenceNode freqs="0" duration="100"></SequenceNode>
-            </ToneSequenceButton>
-        </div>
-        <div class="pad-row">
-            <ToneButton freqs="350,440" :context="audio_context">Dial Tone</ToneButton>
-            <ToneSequenceButton name="Ringback" :context="audio_context">
-                <SequenceNode freqs="440, 480" duration="2000"></SequenceNode>
-                <SequenceNode freqs="0" duration="4000"></SequenceNode>
-            </ToneSequenceButton>            
-        </div>
+        <ToneButton freqs="350,440" :context="audio_context">Dial Tone</ToneButton>
+
+        <ToneSequenceButton name="Busy" :context="audio_context">
+            <SequenceNode freqs="480,620" duration="500"></SequenceNode>
+            <SequenceNode freqs="0" duration="500"></SequenceNode>
+        </ToneSequenceButton>
+
+        <ToneSequenceButton name="Fast Busy" :context="audio_context" :loop="true">
+            <SequenceNode freqs="480,620" duration="250"></SequenceNode>
+            <SequenceNode freqs="0" duration="250"></SequenceNode>
+        </ToneSequenceButton>
+
+        <ToneSequenceButton name="Off Hook" :context="audio_context">
+            <SequenceNode freqs="1440,2060,2450,2600" duration="100"></SequenceNode>
+            <SequenceNode freqs="0" duration="100"></SequenceNode>
+        </ToneSequenceButton>
+
+        <ToneSequenceButton name="Ringback" :context="audio_context">
+            <SequenceNode freqs="440, 480" duration="2000"></SequenceNode>
+            <SequenceNode freqs="0" duration="4000"></SequenceNode>
+        </ToneSequenceButton>
+
+        <ToneSequenceButton name="SIS Ineffective" :context="audio_context">
+            <SequenceNode freqs="914" duration="380"></SequenceNode>
+            <SequenceNode freqs="1429" duration="276"></SequenceNode>
+            <SequenceNode freqs="1777" duration="380"></SequenceNode>
+            <SequenceNode freqs="0" duration="750"></SequenceNode>
+        </ToneSequenceButton>
+
+        <ToneSequenceButton name="SIS Intercept" :context="audio_context">
+            <SequenceNode freqs="914" duration="276"></SequenceNode>
+            <SequenceNode freqs="1371" duration="276"></SequenceNode>
+            <SequenceNode freqs="1777" duration="380"></SequenceNode>
+            <SequenceNode freqs="0" duration="750"></SequenceNode>
+        </ToneSequenceButton>
+
+        <ToneSequenceButton name="SIS No Circuit" :context="audio_context" :loop="true">
+            <SequenceNode freqs="1400, 2060, 2450, 2600" duration="100"></SequenceNode>
+            <SequenceNode freqs="0" duration="100"></SequenceNode>
+        </ToneSequenceButton>
     </div>
+
 </template>
 
 <!-- ----------------------- -->
@@ -55,26 +79,21 @@
 
 #ExtrasUSPanel {
     display: flex;
-    flex-direction: column;
+    flex-wrap:wrap;
     background: linear-gradient(to bottom, #AAA 0%, #333 100%);
     width: 100%;
     height: 100%;
-    font-family: "Bell";    
+    font-family: "Bell";
 
-    .pad-row {
-        flex: 1 1 auto;
-        display: flex;
-
-        .button {
-            // width: 33%;
-            background: linear-gradient(to bottom, #AAA 0%, #EEE 100%);
-            // border-radius: 10%;
-            border: 4px solid rgba(0,0,0,0.05);
-            width: 50%;
-            .label {
-                color: black;
-                font-size: 24pt;
-            }
+    .tone-button,
+    .tone-sequence-button,
+    {
+        flex: 1 1 46%;
+        background: linear-gradient(to bottom, #AAA 0%, #EEE 100%);
+        border: 4px solid rgba(0,0,0,0.05);
+        .label {
+            color: black;
+            font-size: 0.9em;
         }
     }
 }
